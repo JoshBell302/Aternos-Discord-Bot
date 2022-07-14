e6272b7188e48d2afdc69a64c38a6b6287d16496
Josh <55896779+JoshBell302@users.noreply.github.com>
2022-07-13 17:45:36 -0700

    Aternos Connection Working
    Using your Username and Password credentials you can now use and access the server by using the command "!serveron"

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import discord #pip install discord
 import os
+from python_aternos import Client # pip install python-aternos
 client = discord.Client()
 
 # Private information may be censored when posting code 
 TOKEN = '' # Private
-BOT_CHANNEL_NAME = 'bot-test'
+BOT_CHANNEL_NAME = '🍦vanilla-pixelmon-server🍦'
+USERNAME = '' #Private
+PASSWORD = '' # Private
 
 @client.event
 async def on_ready():
@@ -24,7 +27,27 @@ async def on_message(message):
     
     # Commands that only work in a certain channel
     if message.channel.name == BOT_CHANNEL_NAME:
+        # Ping Command
         if command.lower() == '!ping':
             await message.channel.send(f'pong!')
             return
+
+        # Server On Command
+        elif command.lower() == '!serveron':
+            await message.channel.send(f'Turning Server On! It may take a few minutes to be online')
+            aternos = Client.from_credentials(USERNAME, PASSWORD)
+            servs = aternos.list_servers()
+            myserv = servs[0]
+            myserv.start()
+            return
+
+        # Server Off Command
+        elif command.lower() == '!serveroff':
+            await message.channel.send(f'Turning Server Off!')
+            aternos = Client.from_credentials(USERNAME, PASSWORD)
+            servs = aternos.list_servers()
+            myserv = servs[0]
+            myserv.stop()
+            return
+            
 client.run(TOKEN)
